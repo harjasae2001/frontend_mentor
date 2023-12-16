1b7d349282c3803018e8d1c48e3458eafdaf49e9
Harjas Singh <harjassingh_co20b4_07@dtu.ac.in>
2023-12-16 23:53:18 +0530

    Add toggle function to show and hide FAQ content

---
+++ faq-accordion-main/srcipt.js
@@ -0,0 +1,11 @@
+function toggle() {
+    const x = document.getElementById("plus");
+    const p = document.querySelector(".hide")
+    if(p.style.display === 'none'){
+        p.classList.remove("hide");
+        p.classList.add("show");
+    }
+    else{
+
+    }
+}
