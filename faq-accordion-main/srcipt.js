cc8e1ef66568aade38d26eec9c6064657acfe5d5
Harjas Singh <harjassingh_co20b4_07@dtu.ac.in>
2023-12-17 17:50:26 +0530

    Fix FAQ accordion toggle functionality

--- faq-accordion-main/srcipt.js
+++ faq-accordion-main/srcipt.js
@@ -1,11 +1,24 @@
-function toggle() {
-    const x = document.getElementById("plus");
-    const p = document.querySelector(".hide")
-    if(p.style.display === 'none'){
-        p.classList.remove("hide");
-        p.classList.add("show");
+
+
+
+var img = document.getElementsByClassName('toggleBtn')
+
+for(let i=0; i<img.length; i++){
+    img[i].addEventListener('click', () => {
+    var p = document.querySelectorAll('p');
+    const img = document.querySelector('.toggleBtn');
+
+    if(p[i].classList.contains("hide") === true){
+        p[i].classList.remove("hide");
+        p[i].classList.add("show");
     }
     else{
-
+        p[i].classList.remove("show");
+        p[i].classList.add("hide");
+    
     }
+    // p.classList.toggle("hide");
+    console.log(img.classList.src);
+    img.classList.src = "faq-accordion-main/assets/images/icon-minus.svg"
+});
 }
