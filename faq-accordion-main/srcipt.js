055b2c5d04f0dfedf83b1589ad2c8039d71f1d6c
Harjas Singh <harjassingh_co20b4_07@dtu.ac.in>
2023-12-17 18:46:34 +0530

    Fix toggle button functionality

--- faq-accordion-main/srcipt.js
+++ faq-accordion-main/srcipt.js
@@ -6,7 +6,7 @@ var img = document.getElementsByClassName('toggleBtn')
 for(let i=0; i<img.length; i++){
     img[i].addEventListener('click', () => {
     var p = document.querySelectorAll('p');
-    // var img = document.querySelector('.toggleBtn');
+
 
     if(p[i].classList.contains("hide") === true){
         p[i].classList.remove("hide");
