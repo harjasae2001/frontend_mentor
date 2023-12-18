60ee8b651a7e34af69c52d36895a27716f18c234
Harjas Singh <harjassingh_co20b4_07@dtu.ac.in>
2023-12-18 13:40:09 +0530

    Refactor FAQ accordion functionality

--- faq-accordion-main/srcipt.js
+++ faq-accordion-main/srcipt.js
@@ -1,12 +1,8 @@
-
-
-
 var img = document.getElementsByClassName('toggleBtn')
 
 for(let i=0; i<img.length; i++){
     img[i].addEventListener('click', () => {
     var p = document.querySelectorAll('p');
-
 
     if(p[i].classList.contains("hide") === true){
         p[i].classList.remove("hide");
@@ -17,12 +13,12 @@ for(let i=0; i<img.length; i++){
         p[i].classList.add("hide");
     
     }
-
-    if(img[i].getAttribute('src') === "./assets/images/icon-plus.svg"){
-        img[i].setAttribute('src', "./assets/images/icon-minus.svg");
+    var im = document.querySelectorAll('img');
+    if(im[i+1].getAttribute('src') === "./assets/images/icon-plus.svg"){
+        im[i+1].setAttribute('src', "./assets/images/icon-minus.svg");
     }
-    else{
-        img[i].setAttribute('src', "./assets/images/icon-plus.svg");
+    else if(im[i+1].getAttribute('src') === "./assets/images/icon-minus.svg"){
+        im[i+1].setAttribute('src', "./assets/images/icon-plus.svg");
     }
 });
 }
